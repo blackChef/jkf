395a7b58dba57d9cdad97d36182329411d4c1749
blackChef <hongpidai@gmail.com>
2016-04-13 15:07:04 +0800

    transform combination keep previous state
    only transform defined in inline style can be keeped

--- src/index.jsx
+++ src/index.jsx
@@ -29,11 +29,25 @@ registerCombination({
   check: function(propName) {
     return propName.match(/translate|rotate|scale|skew/);
   },
-  combine: function(values) {
-    var ret = values.map(function(item) {
-      return item.propName + '(' + item.value + ')';
-    });
-    return ret.join(' ');
+  combine: function(values, elem) {
+    // translateY(10px) translateX(30px)
+    var currentStyle = elem.style[prefix('transform')].split(' ')
+        .reduce(function(preVal, curItem) {
+          var [, key, value] = curItem.match(/(\w*?)\((.*)\)/);
+          return Object.assign(preVal, { [key]: value });
+        }, {});
+
+    var newStyle = values.reduce(function(preVal, curItem) {
+      return Object.assign(preVal, { [curItem.propName]: curItem.value });
+    }, {});
+
+    var resovledStyle = Object.assign({}, currentStyle, newStyle);
+
+    var ret = Object.keys(resovledStyle).map(function(key) {
+      return `${key}(${resovledStyle[key]})`;
+    }).join(' ');
+
+    return ret;
   }
 });
 
