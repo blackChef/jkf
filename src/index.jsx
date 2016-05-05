45ae2afb4693dad9e973cf401ebe76e9113e0beb
blackChef <hongpidai@gmail.com>
2016-05-05 17:34:51 +0800

    fix transform combination

--- src/index.jsx
+++ src/index.jsx
@@ -31,11 +31,16 @@ registerCombination({
   },
   combine: function(values, elem) {
     // translateY(10px) translateX(30px)
-    var currentStyle = elem.style[prefix('transform')].split(' ')
-        .reduce(function(preVal, curItem) {
-          var [, key, value] = curItem.match(/(\w*?)\((.*)\)/);
-          return Object.assign(preVal, { [key]: value });
-        }, {});
+    var currentStyle = {};
+    if (elem.style[prefix('transform')].trim()) {
+      currentStyle = elem
+          .style[prefix('transform')]
+          .split(' ')
+          .reduce(function(preVal, curItem) {
+            var [, key, value] = curItem.match(/(\w*?)\((.*)\)/);
+            return Object.assign(preVal, { [key]: value });
+          }, {});
+    }
 
     var newStyle = values.reduce(function(preVal, curItem) {
       return Object.assign(preVal, { [curItem.propName]: curItem.value });
